b93ee7ecf48a7fd1d6dd512283f19b4763f01831
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-05-04 10:22:39 -0600

    I3: guessing game complete

--- web-guesser.rb
+++ web-guesser.rb
@@ -1,10 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(101)
+SECRET_NUMBER = rand(101)
+message = ""
+number_message = ""
+
+def check_guess(guess)
+  if guess > SECRET_NUMBER + 5
+    message = "Way too high!"
+  elsif guess > SECRET_NUMBER && guess.to_i  <= SECRET_NUMBER + 5
+    message = "Too high!"
+  elsif guess == SECRET_NUMBER
+    message = "You got it right! \n The SECRET NUMBER is #{SECRET_NUMBER}"
+    # number_message = "The SECRET NUMBER is #{SECRET_NUMBER}"
+    # message + '/n' + number_message
+  elsif guess < SECRET_NUMBER && guess >= SECRET_NUMBER - 5
+    message = "Too low!"
+  else
+    message = "Way too low!"
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:number => number}
-  #render the erb template named index and create a local variable for the template
-  #named number which has the same value as the number variable from this server code 
+  guess = params['guess'].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:number_message => number_message, :message => message}
 end
